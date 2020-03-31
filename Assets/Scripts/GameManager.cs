6cb4a43083fe87f4750d9a7d95969862da2ec7a2
Willowcrickit <willowcrickit@gmail.com>
2020-03-31 01:35:13 -0700

    3/31/20

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int currentScene = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            Debug.Log("Alert: More than one Game Manager detected, Deleting extra(s)");
+        }
+    }
+
+    public void LoadLevel(string levelToLoad)
+    {//Danger, Maybe don't use
+        SceneManager.LoadScene(levelToLoad);
+        currentScene++;
+    }
+
+    public void LoadLevel(int indexToLoad)
+    {
+        SceneManager.LoadScene(indexToLoad);
+        currentScene = indexToLoad;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+        currentScene++;
+    }
+}
